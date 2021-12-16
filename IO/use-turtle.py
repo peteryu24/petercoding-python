a34c24a46072729e730ac56e9b63db5a4e2545f9
Peter Yu <peteryu24@naver.com>
2021-12-17 01:09:26 +0900

    using turtle with event function

--- IO/use-turtle.py
+++ IO/use-turtle.py
@@ -37,4 +37,26 @@ turtle.onscreenclick(screenLeftClick, 1) #Event Function
 turtle.done()
 // draws wherever the mouse clicks
 
+import turtle
+
+aaa = turtle.Turtle()
+sc = turtle.Screen()
+turtle.title('Click left mouse')
+
+sc.setup(500,500, startx = 0, starty = 0)
+
+aaa.speed(100)
+def screenLeftClick(x, y) :    
+    aaa.pendown()
+    aaa.goto(x, y)
+    aaa.penup()
+
+def screenRightClick(x, y):
+    aaa.clear() #erase everything
+    
+turtle.onscreenclick(screenLeftClick, 1)
+turtle.onscreenclick(screenRightClick, 3) #register right click
+
+turtle.done()
+
 
